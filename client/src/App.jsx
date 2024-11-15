69422fb3cf614a7290507123e0c86f13402f2f6b
nikhil chandurkar <nikhilchandurkar24@gmail.com>
2024-11-15 15:13:54 +0530

    "  "

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,41 +3,46 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import ProtectRoute from "./components/auth/ProtectRoute";
 import { LayoutLoader } from "./components/layout/Loaders";
 
-const Home = lazy(() => import("./pages/Home"))
-const Login = lazy(() => import("./pages/Login"))
-const Chat = lazy(() => import("./pages/Chat"))
-const Groups = lazy(() => import("./pages/Groups"))
-const NotFound = lazy(()=> import ("./pages/NotFound"))
-// let user = false;
-let user = true;
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+
+let user = true; // Simulating user authentication status
 
 const App = () => {
   return (
-
     <BrowserRouter>
-      <Suspense fallback={<LayoutLoader/>}>
-      <Routes>
-        {/* protected routes if log in then access it else other /login  */}
-        <Route element={<ProtectRoute user={user} />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/groups" element={<Groups />} />
-          <Route path="/chat/:chatid" element={<Chat />} />
-          {/* <Route path="/chat" element={<Chat />} /> */}
-        </Route>
-        {/* route to login  */}
-        <Route path="/login" element={<ProtectRoute user={!user}>
-          <Login />
-        </ProtectRoute>} />
-        {/* route to 404 page */}
+      <Suspense fallback={<LayoutLoader />}>
+        <Routes>
+          {/* Protected routes - accessible only for logged-in users */}
+          <Route element={<ProtectRoute user={user} />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/groups" element={<Groups />} />
+            <Route path="/chat/:chatid" element={<Chat />} />
+          </Route>
 
-        <Route path="*" element={<NotFound />} />
+          {/* Login route, but if user is already logged in, redirect them to home */}
+          <Route
+            path="/login"
+            element={
+              user ? (
+                <ProtectRoute user={user}>
+                  <Home /> {/* Redirect to Home if already logged in */}
+                </ProtectRoute>
+              ) : (
+                <Login />
+              )
+            }
+          />
 
-      </Routes>
-
-
+          {/* Route to 404 page */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </Suspense>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
